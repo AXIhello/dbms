74bdbdf1ac0467886190b783c8a782157ac8ab95
cat-cat-web <changbai_2023@163.com>
2025-04-21 10:06:41 +0800

    支持解析group by ,order by。但目前插入似乎有bug

--- parse_DML.cpp
+++ parse_DML.cpp
@@ -29,14 +29,12 @@ void Parse::handleUpdate(const std::smatch& m) {
     std::string setClause = m[2];  // SET 子句
     std::string condition = m.size() > 3 ? m[3].str() : "";
 
-    std::cout << "UPDATE 表名: " << tableName << std::endl;
-    std::cout << "SET 子句: " << setClause << std::endl;
-    std::cout << "WHERE 条件: " << condition << std::endl;
+    std::string table_path = dbManager::getInstance().getCurrentDatabase()->getDBPath() + "/" + tableName;
 
     // 创建 Record 对象来执行更新操作
     Record record;
     try {
-        record.update(tableName, setClause, condition);
+        record.update(table_path, setClause, condition);
 
         Output::printMessage(outputEdit, QString::fromStdString("UPDATE 执行成功。"));
     }
@@ -59,7 +57,7 @@ void Parse::handleDelete(const std::smatch& m) {
         std::string table_path = dbManager::getInstance().getCurrentDatabase()->getDBPath() + "/" + table_name;
 
         Record r;
-        r.delete_(table_name, condition);
+        r.delete_(table_path, condition);
 
         Output::printMessage(outputEdit, QString::fromStdString("DELETE FROM 执行成功。"));
     }
