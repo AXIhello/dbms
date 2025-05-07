0ffb45e22d8362bcf91347a865f5f68a90431fb0
Wangzihan0506 <1479679245@qq.com>
2025-05-07 22:06:17 +0800

    终端移植在cmd中运行

--- manager/dbManager.cpp
+++ manager/dbManager.cpp
@@ -11,25 +11,6 @@
 namespace fs = std::filesystem;
 std::string dbManager::basePath = std::filesystem::current_path().string() + "/DBMS_ROOT";
 
-/*std::string Utf8ToGbk1(const std::string& utf8)
-{
-    int len = MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), -1, NULL, 0);
-    if (len == 0) return "";
-
-    std::wstring wstr(len, 0);
-    MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), -1, &wstr[0], len);
-
-    len = WideCharToMultiByte(936, 0, wstr.c_str(), -1, NULL, 0, NULL, NULL);
-    if (len == 0) return "";
-
-    std::string gbk(len, 0);
-    WideCharToMultiByte(936, 0, wstr.c_str(), -1, &gbk[0], len, NULL, NULL);
-
-    if (!gbk.empty() && gbk.back() == '\0') gbk.pop_back();
-    return gbk;
-}
-*/
-
 
 // 构造函数
 dbManager::dbManager() {
@@ -135,33 +116,7 @@ void dbManager::remove_database_info(const std::string& db_name)
         outFile.close();
 }
 
-// 加载系统数据库信息(未完成。应该从.db文件中读取,但不知道有什么用……)
-/*void dbManager::load_system_db_info() {
-    std::string sysDBPath = basePath + "/" + systemDBFile;
-   // std::cout << Utf8ToGbk1("读取系统数据库文件: ") << sysDBPath << std::endl;
 
-    std::ifstream sysDBFile(sysDBPath, std::ios::binary);
-    if (!sysDBFile) {
-        throw std::runtime_error("无法读取系统数据库文件 ruanko.db");
-    }
-
-    DatabaseBlock dbInfo;
-    int count = 0;
-    while (sysDBFile.read(reinterpret_cast<char*>(&dbInfo), sizeof(DatabaseBlock))) {
-        std::string dbName(dbInfo.dbName, strnlen(dbInfo.dbName, sizeof(dbInfo.dbName)));
-        std::string dbPath(dbInfo.filepath, strnlen(dbInfo.filepath, sizeof(dbInfo.filepath)));
-        std::string typeStr = dbInfo.type ? Utf8ToGbk1("用户数据库") : Utf8ToGbk1("系统数据库");
-        std::time_t t = dbInfo.crtime;
-        char timebuf[64] = { 0 };
-        std::tm tm_buf;
-        localtime_s(&tm_buf, &t);
-        std::strftime(timebuf, sizeof(timebuf), "%Y-%m-%d %H:%M:%S", &tm_buf);
-
-       
-        ++count;
-    }
-    sysDBFile.close();
-*/
 void dbManager::load_system_db_info() {
     std::string sysDBPath = basePath + "/" + systemDBFile;
     std::ifstream sysDBFile(sysDBPath, std::ios::binary);
@@ -388,7 +343,6 @@ bool dbManager::database_exists_in_db(const std::string& db_name) {
     DatabaseBlock block;
     while (file.read(reinterpret_cast<char*>(&block), sizeof(DatabaseBlock))) {
         std::string name(block.dbName, strnlen(block.dbName, sizeof(block.dbName)));
-        //std::cout << "比对: [" << name << "] <-> [" << db_name << "]" << std::endl;
         if (name == db_name) {
             file.close();
             return true;
